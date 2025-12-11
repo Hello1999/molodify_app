801ab796b38731c24834e93879e1938c5a1db5b1
yanwenjiie <yanwenjie21@outlook.com>
2025-12-12 01:20:52 +0800

    构建了底部导航栏组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:melodify_app/core/theme/app_theme.dart';
 import 'package:melodify_app/pages/app_shell.dart';
+import 'package:melodify_app/providers/providers.dart';
+import 'package:provider/provider.dart';
 
 void main(List<String> args) async {
-  runApp(MelodifyApp());
+  final themeProvider = ThemeProvider();
+  themeProvider.loadSettings();
+
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider.value(value: themeProvider)],
+      child: MelodifyApp(),
+    ),
+  );
 }
 
 class MelodifyApp extends StatelessWidget {
@@ -10,9 +21,13 @@ class MelodifyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = context.watch<ThemeProvider>();
+
     return MaterialApp(
       title: 'Melodify',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme(themeProvider.seedColor),
+      darkTheme: AppTheme.darkTheme(themeProvider.seedColor),
       home: AppShell(),
     );
   }
--- lib/pages/app_shell.dart
+++ lib/pages/app_shell.dart
@@ -9,10 +9,51 @@ class AppShell extends StatefulWidget{
 }
 
 class _AppShellState extends State<AppShell> {
+  int _currentIndex = 0;
+
+
+  final List<NavigationDestination> _destinations = const [
+    NavigationDestination(
+      icon: Icon(Icons.home_outlined),
+      selectedIcon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    NavigationDestination(
+      icon: Icon(Icons.search_outlined),
+      selectedIcon: Icon(Icons.search),
+      label: 'Search',
+    ),
+    NavigationDestination(
+      icon: Icon(Icons.library_music_outlined),
+      selectedIcon: Icon(Icons.library_music),
+      label: 'Library',
+    ),
+    NavigationDestination(
+      icon: Icon(Icons.person_outlined),
+      selectedIcon: Icon(Icons.person),
+      label: 'Profile',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('data'),
+      body: Row(
+        children: [
+          Text('A')
+        ],
+      ),
+      bottomNavigationBar: Column(
+        children: [
+          NavigationBar(
+            selectedIndex: _currentIndex, onDestinationSelected: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+          },
+          destinations: _destinations, 
+          )
+        ],
+      ),
     );
   }
 }
