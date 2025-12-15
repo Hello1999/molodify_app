507e94c39488e353d9ebf518327f1f13b4d0195f
yanwenjiie <yanwenjie21@outlook.com>
2025-12-16 00:16:48 +0800

    okay

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,9 +1,100 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  String _getGreeting() {
+    final hour = DateTime.now().hour;
+    if (hour < 12) return 'Good morning';
+    if (hour < 18) return 'Good afternoon';
+    return 'Good evening';
+  }
+
+
+
+  Widget _buildQuickPlayGrid(BuildContext context,) {
+    return Padding(padding: EdgeInsets.symmetric(horizontal: 16), 
+    child: GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        childAspectRatio: 3.5,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8
+      ),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Text('G');
+      },
+    ),);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(body: Text('Home'));
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            title: Row(
+              children: [
+                Icon(
+                  Icons.music_note_rounded,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: 8),
+                Text('Melodify'),
+              ],
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.notifications_outlined),
+              ),
+              IconButton(onPressed: () {}, icon: Icon(Icons.history)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.settings_outlined)),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                _getGreeting(),
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+
+          SliverToBoxAdapter(
+             child: _buildQuickPlayGrid(context),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class _QuickPlayItem extends StatelessWidget {
+  final dynamic playlist;
+  final VoidCallback? onTap;
+
+  _QuickPlayItem({
+    required this.playlist,
+    this.onTap
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme = Theme.of(context).colorScheme;
+
+    return Material(
+      color: ColorScheme.surfaceContainerHighest.withValues(alpha: 0.5),
+      borderRadius: BorderRadius.circular(4),
+
+    );
   }
 }
